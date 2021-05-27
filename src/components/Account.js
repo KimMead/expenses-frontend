3868d8417fccc3c04d77df3ee202c481e6470388
KimMead <kimmeadows8@gmail.com>
2021-05-26 21:15:07 -0500

    add router

---
+++ src/components/Account.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Account = (props) => {
+
+return (
+<li>
+    {props.account.name} - {props.account.balance}
+</li>
+
+)
+}
+
+export default Account
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import {BrowserRouter as Router} from 'react-router-dom'
 import accountReducer from './reducers/accountReducer'
 
 import App from './App';
@@ -14,9 +15,10 @@ let store = createStore(accountReducer, composeEnhancers(applyMiddleware(thunk))
 
 ReactDOM.render(
 <Provider store={store}>
-<App />
-</Provider>
-, 
+    <Router>
+        <App />
+    </Router>
+</Provider>, 
 document.getElementById('root'));
 
 
